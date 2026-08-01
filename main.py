9084dd52e5b5c15a24d70d3a876bea0830110d54
Vikas Chaurasia <getintouchwithvic@gmail.com>
2026-08-01 20:00:47 +0530

    animated

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import sys
+import random
 import pygame
 import math
+import time
+from functools import lru_cache
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1600
+SCREEN_HEIGHT = 1200
 FPS = 60
 
 PI = math.pi
-BG_COLOR = (30, 30, 30)
+BG_COLOR = (255, 255, 255)
 RED = (255, 30, 30)
 GREEN = (30, 255, 30)
 BLUE = (30, 40, 255)
@@ -23,25 +26,26 @@ clock = pygame.time.Clock()
 
 
 class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, x, y, precision=6):
+        self.x = round(float(x), precision)
+        self.y = round(float(y), precision)
 
 
     def __hash__(self): 
         # this is critical to avoid floating point errors
         # other wise it will create hexagons infinitely close, but not quite
         # at the same positions. Ruining a lifetime debugging.
+        # return hash(
+        #     self.ival
+        # )
         return hash(
-            self.ival
+            (self.x, self.y)
         )
 
     def __eq__(self, other):
         if isinstance(other, Point):
-            x, y = self.ival
-            _x, _y = other.ival
-            return _x == x and _y == y
-        raise Exception(f"{other} is not a Point type")
+            return self.x == other.x and self.y == other.y
+        return False
 
     def dist(self, p: Point):
         if not isinstance(p, Point):
@@ -75,8 +79,8 @@ class Point:
         return self + p
 
     def __repr__(self):
-        # return f"Point({self.x}, {self.y})"
-        return f"Point({self.ival})"
+        return f"Point({self.x}, {self.y})"
+        # return f"Point({self.ival})"
 
 
     def rotate(self, radians):
@@ -97,6 +101,8 @@ class Point:
 
 class Hexagon:
     def __init__(self, pos: Point, size, omit=(),color=RED):
+        if not isinstance(pos, Point):
+            raise Exception(f"{pos} is not a Point")
         self.pos = pos
         self.size = size
         self.omit = omit
@@ -148,8 +154,8 @@ class Hexagon:
         ]
 
 
-        pygame.draw.circle(screen, BLUE, screen_points[0].val, radius=8, width=2)
-        pygame.draw.circle(screen, BLUE, screen_points[1].val, radius=12, width=2)
+        # pygame.draw.circle(screen, BLUE, screen_points[0].val, radius=8, width=2)
+        # pygame.draw.circle(screen, BLUE, screen_points[1].val, radius=12, width=2)
         for i in range(1, len(screen_points)):
             if (((i-1)-3) % 6)+1 in self.omit:
                 continue
@@ -181,31 +187,6 @@ def n(p: Point):
     return p + Point(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
 
-def nested_hexagon(grid_x=5, grid_y=15):
-    size = 25
-    positions = []
-    # origin = n(Point(0, 0))
-    origin = Point(0, 0)
-
-    for x in range(1, grid_x + 1):
-        for y in range(1, grid_y + 1):
-            # yesh
-            base_x = origin.x
-            base_y = origin.y
-
-            _x = 3 * size * x + (size * 3/2 * (y % 2 == 0)) + base_x
-            _y = math.sin(2*PI/6)  * 1* size * y + base_y
-            positions.append(Point(_x, _y))
-
-    hexes = []
-    for pos in positions:
-        hexes.append(
-            Hexagon(pos, size)
-        )
-
-    print(f"hexes: {len(hexes)}")
-    return hexes
-
 
 
 
@@ -225,8 +206,9 @@ def nested_hexagon(grid_x=5, grid_y=15):
 
 
 class Graph:
-    def __init__(self):
+    def __init__(self, hex_size=25):
         self.data = {}
+        self.hex_size = hex_size
 
     def __repr__(self):
         res = "Graph:\n"
@@ -240,20 +222,25 @@ class Graph:
         # a very, very basic check to see if there's
         # two nodes that are TOO damn close to each other.
         # O(n^2) but should be fine since my graphs are tiny
-        for p1 in self.data.keys():
-            for p2 in self.data.keys():
-                if p1 == p2:
-                    continue
-                else:
-                    if p1.dist(p2) < 1.0:
-                        raise Exception(
-                            f"""
+        nodes = list(self.data.keys())
+        for i in range(len(nodes)):
+            for j in range(i+1, len(nodes)):
 
-                            There's some duplicate node too close to another
-                                {p1} <> {p1}
+                p1 = nodes[i]
+                p2 = nodes[j]
 
-                            """
-                        )
+                if p1.dist(p2) < 1.0:
+                    raise Exception(
+                        f"""
+
+                        There's some duplicate node too close to another
+                            {p1} \n<>\n {p2}
+                        {self.data}
+                        \n\n
+                        { self }
+
+                        """
+                    )
         print("Graph invariant is fine")
                     
         
@@ -280,8 +267,9 @@ class Graph:
 
         return (((n-1)+3) % 6) + 1 # +1 to bring back to 1-6 range
 
+    @lru_cache
     def get_neighbour_offset(self, n) -> Point:
-        base_hex = Hexagon(Point(0,0), size=25)
+        base_hex = Hexagon(Point(0,0), size=self.hex_size)
         hypotenuse_length = math.sqrt(3)# approximation
         rotation = 2 * PI / 12
 
@@ -301,20 +289,18 @@ class Graph:
         return rotated_pos_offset
 
     def get_neighbour(self, node: Point, n, add=False) -> Point:
+        if not isinstance(node, Point):
+            raise Exception(f"{node} node better be a point")
         neighbour_offset = self.get_neighbour_offset(n)
         neighbour_pos = neighbour_offset.translate(node)
         if not add and neighbour_pos not in self.data.keys():
             raise Exception(f"{node} does not have {n} as a neighbour")
         return neighbour_pos
 
-    def get_neighbour_connections(self, node):
-
+    def get_neighbour_connections(self, node: Point):
+        if not isinstance(node, Point):
+            raise Exception(f"{node}: node must be a point")
         return self.data.get(node)
-
-
-
-
-
 
     def add_neighbour(self, node: Point, n):
         if n < 1 or n > 6:
@@ -334,6 +320,21 @@ class Graph:
         else:
             self.data[neighbour_pos] = set([self.mirror(n)])
 
+    def get_hexagons(self):
+        hexes = []
+        for point in self.data:
+            if not isinstance(point, Point):
+                raise Exception("what are you doing")
+            
+            hexes.append(
+                Hexagon(
+                    point, 
+                    size=self.hex_size, 
+                    omit=self.get_neighbour_connections(point),
+                    # color = (int(point.x) % 256, int(point.y) % 256, 250)
+                )
+            )
+        return hexes
 
 
 
@@ -341,59 +342,80 @@ class Graph:
 
 
 
-graph = Graph()
-start = Point(0,0)
+# graph = Graph()
+# start = Point(0,0)
+#
+# graph.add_neighbour(start, 1)
+# graph.add_neighbour(start, 2)
+# # graph.add_neighbour(start, 3)
+#
+# two = graph.get_neighbour(start, 2)
+# print("Two", two)
+# graph.add_neighbour(two, 2)
+#
+# graph.add_neighbour(two, 5)
+# graph.add_neighbour(two, 6)
+#
+# three = graph.get_neighbour(two, 2)
+# graph.add_neighbour(three, 2)
+#
+# four = graph.get_neighbour(three, 2)
+# graph.add_neighbour(four, 2)
+#
+# five = graph.get_neighbour(four, 2)
+# graph.add_neighbour(five, 2)
+#
+# six = graph.get_neighbour(four, 2)
+# graph.add_neighbour(six, 6)
+#
+# seven = graph.get_neighbour(six, 6)
+# graph.add_neighbour(seven, 6)
+#
+# eight = graph.get_neighbour(seven, 6)
+# graph.add_neighbour(seven, 5)
+#
+#
+# graph.floating_point_precision_check()
+#
+# print(graph)
+#
 
-graph.add_neighbour(start, 1)
-graph.add_neighbour(start, 2)
-# graph.add_neighbour(start, 3)
-
-two = graph.get_neighbour(start, 2)
-print("Two", two)
-graph.add_neighbour(two, 2)
-
-graph.add_neighbour(two, 5)
-graph.add_neighbour(two, 6)
-
-three = graph.get_neighbour(two, 2)
-graph.add_neighbour(three, 2)
-
-four = graph.get_neighbour(three, 2)
-graph.add_neighbour(four, 2)
-
-five = graph.get_neighbour(four, 2)
-graph.add_neighbour(five, 2)
-
-six = graph.get_neighbour(four, 2)
-graph.add_neighbour(six, 6)
-
-seven = graph.get_neighbour(six, 6)
-graph.add_neighbour(seven, 6)
-
-eight = graph.get_neighbour(seven, 6)
-graph.add_neighbour(seven, 5)
+def generate_random_hexagon_maze(n: int):
+    if not isinstance(n, int):
+        raise Exception("dont be an idiot")
 
 
-graph.floating_point_precision_check()
+    g = Graph()
+
+    node = Point(0,0)
+    nodes = [node]
+    decisions = []
+    for x in range(n):
+        _n = random.randint(1,6)
+        g.add_neighbour(node, _n)
+        decisions.append(_n)
+        cur_neighbours = g.get_neighbour_connections(node)
+        choice = random.choice(list(cur_neighbours))
+
+        node = g.get_neighbour(node, choice)
+        nodes.append(node)
 
 
-# THIS ONE IS BROKEN, probably a floating point math issue
-# graph.add_neighbour(three, 5)
+    print("Decisions:")
+    print(decisions)
+    print("nodes:")
+    print(nodes)
+
+    return g
+
+
+        
 
 
 
 
 
 
-# graph.add_neighbour(start, 3)
-# graph.add_neighbour(start, 4)
-# graph.add_neighbour(start, 5)
-# graph.add_neighbour(start, 6)
-
-# second = graph.data.get(start)
-# graph.
-
-print(graph)
 
 
 
@@ -407,19 +429,19 @@ print(graph)
 
 
 
-hexes = []
-for point in graph.data:
-    if not isinstance(point, Point):
-        raise Exception("what are you doing")
-    
-    hexes.append(
-        Hexagon(
-            point, 
-            size=25, 
-            omit=graph.get_neighbour_connections(point),
-            # color = (int(point.x) % 256, int(point.y) % 256, 250)
-        )
-    )
+# hexes = []
+# for point in graph.data:
+#     if not isinstance(point, Point):
+#         raise Exception("what are you doing")
+#
+#     hexes.append(
+#         Hexagon(
+#             point, 
+#             size=25, 
+#             omit=graph.get_neighbour_connections(point),
+#             # color = (int(point.x) % 256, int(point.y) % 256, 250)
+#         )
+#     )
 
 
 
@@ -453,6 +475,12 @@ for point in graph.data:
 
 
 
+G = Graph(hex_size=15)
+
+node = Point(0,0)
+nodes = [node]
+decisions = []
+
 time = 0
 while running:
 
@@ -469,6 +497,24 @@ while running:
 
 
     time +=1 
+
+    # G = generate_random_hexagon_maze(time)
+
+
+    bias_factor = 5
+    _n = random.randint(1,6)
+    for x in range(bias_factor):
+        G.add_neighbour(node, _n)
+        decisions.append(_n)
+        cur_neighbours = G.get_neighbour_connections(node)
+        choice = random.choice(list(cur_neighbours))
+        node = G.get_neighbour(node, choice)
+        nodes.append(node)
+
+
+    G.floating_point_precision_check()
+    hexes = G.get_hexagons()
+    print("TIME:", time)
 
     
     screen.fill(BG_COLOR)
